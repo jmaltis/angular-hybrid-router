e5e426be08e2ae5d422f21629368da65fca1ef1d
Jeremy Maltis <jeremy.maltis@amadeus.com>
2019-06-04 14:07:41 -0400

    Add valdr angularJs dependency

--- src/ng-app/app.module.ajs.ts
+++ src/ng-app/app.module.ajs.ts
@@ -1,3 +1,25 @@
 import * as angular from "angular";
 
-export default angular.module("angularjsModule", []);
+var angularjsModule = angular.module("angularjsModule", [
+    "valdr"
+]);
+
+angularjsModule.run(["valdr", "valdrMessage",
+    function (valdr, valdrMessage) {
+        // Enables usage of valdrMessages
+        valdrMessage.angularMessagesEnabled = true;
+
+        // Add some fake valdr constraints
+        valdr.addConstraints({
+            InputTestConstraint: {
+                testField: {
+                    required: {
+                        message: "This field is required"
+                    }
+                }
+            }
+        });
+    }
+]);
+
+export default angularjsModule;
